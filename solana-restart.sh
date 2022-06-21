cb81db494b8b089fc92ac055145cdff0df243619
Thorsten Behrens <tbehrens@outlook.com>
2022-06-21 08:50:30 +0000

    Honor Ctrl-C when monitoring restart

--- solana-restart.sh
+++ solana-restart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Safely restart Solana by waiting for empty leader slot and delinquency
+set -e
+
 cd /home/sol
 sudo -u sol /home/sol/.local/share/solana/install/active_release/bin/solana-validator exit -m
 sudo -u sol /home/sol/.local/share/solana/install/active_release/bin/solana-validator monitor
