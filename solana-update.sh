e083c5d50a00540956cbbabf01003ba4725b55d6
Thorsten Behrens <tbehrens@outlook.com>
2022-06-21 08:37:38 +0000

    Convenient update from outside sol user

---
+++ solana-update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Update Solana without needing to sudo into sol
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 DESIRED-VERSION"
+  exit 0
+fi
+
+sudo -u sol /home/sol/.local/share/solana/install/active_release/bin/solana-install init $1
