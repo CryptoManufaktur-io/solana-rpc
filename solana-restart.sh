11be1d6fef9072c3c8da21bad4173ce9f3402131
Thorsten Behrens <tbehrens@outlook.com>
2022-06-21 08:47:27 +0000

    More elegant restart

--- solana-restart.sh
+++ solana-restart.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Safely restart Solana by waiting for empty leader slot and delinquency
 cd /home/sol
-sudo -u sol /home/sol/.local/share/solana/install/active_release/bin/solana-validator wait-for-restart-window
-sudo systemctl restart validator
+sudo -u sol /home/sol/.local/share/solana/install/active_release/bin/solana-validator exit -m
 sudo -u sol /home/sol/.local/share/solana/install/active_release/bin/solana-validator monitor
