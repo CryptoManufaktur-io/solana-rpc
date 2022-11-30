98718203b39e1dd5350d2ed2293b6c428fe66e9e
Thorsten Behrens <tbehrens@outlook.com>
2022-11-30 18:10:47 +0000

    home sol ledger must exist

--- solana-get-snapshots.sh
+++ solana-get-snapshots.sh
@@ -2,6 +2,7 @@
 # Only when required, manually grab snapshots from Solana labs
 set -e
 
+sudo -u sol mkdir -p /home/sol/ledger
 cd /home/sol/ledger
 sudo -u sol wget --trust-server-names http://145.40.67.83/snapshot.tar.bz2
 sudo -u sol wget --trust-server-names http://145.40.67.83/incremental-snapshot.tar.bz2
